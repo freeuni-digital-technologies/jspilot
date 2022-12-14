c77eaeb3244353c56947611b41e55a223c58ac93
ia rigby <marvinzem@gmail.com>
2022-12-14 22:06:38 +0400

    lol

--- scripts/createZips.sh
+++ scripts/createZips.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 rm -r dist 2> /dev/null
-for hw in $(/bin/ls homeworks | grep -v "teststyles.css")
+for hw in $(/bin/ls homeworks | grep -v "teststyles.css" | grep -v "static")
 do
 	mkdir -p "dist/$hw"
   find_string="<script type='module' src='./main.js'>"
