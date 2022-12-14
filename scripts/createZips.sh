c26555a773b1a8392964d1ecce235e766774811e
ia rigby <marvinzem@gmail.com>
2022-12-14 22:04:38 +0400

    lol

--- scripts/createZips.sh
+++ scripts/createZips.sh
@@ -7,7 +7,7 @@ do
   find_string="<script type='module' src='./main.js'>"
   replace_string="<script type='text/javascript' src='http://freeuni-digital-technologies.github.io/jspilot/$hw/main.js'>"
   sed -i.bak "s~$find_string~$replace_string~" "homeworks/$hw/index.html" && rm "homeworks/$hw/index.html.bak"
-	git checkout "homeworks/$hw/index.html"
 	zip -rj "dist/$hw/$hw.zip" "homeworks/$hw/" -x \
 		\*solution\* \*server.js \*extras.js \*tests.js \*main.js \*animation.js
+  git checkout "homeworks/$hw/index.html"
 done
